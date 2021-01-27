dfa0dc69463cbd22a808d51bf9980a6f7e4c3290
bruno <brunopaes05@gmail.com>
2021-01-27 20:00:12 -0300

    Adding cnn wawaweewa

---
+++ src/fat_secret.py
@@ -0,0 +1,8 @@
+from fatsecret import Fatsecret
+
+consumer_key = '7364306f18a4410ebd067d2c1b347f19'
+consumer_secret = '561f4121edfb4e00bbb8cebfd9ee29f8'
+
+fs = Fatsecret(consumer_key, consumer_secret)
+
+fs.foods_search
