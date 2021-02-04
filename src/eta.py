7fa913bcbf6934fb16c05fbe2cf1834f289b0af7
bruno <brunopaes05@gmail.com>
2021-02-04 14:51:21 -0300

    Removing fatsecret

--- src/eta.py
+++ src/eta.py
@@ -4,11 +4,11 @@ import datetime
 
 class CalcETA:
     def __call__(self, *args, **kwargs):
-        avengers = \
-            datetime.datetime.today().replace(day=9, month=12, year=2020,
-                                              hour=21, minute=00, second=0,
+        eta = \
+            datetime.datetime.today().replace(day=6, month=2, year=2021,
+                                              hour=10, minute=30, second=0,
                                               microsecond=0)
-        date = abs(datetime.datetime.today() - avengers)
+        date = abs(datetime.datetime.today() - eta)
 
         m, s = divmod(date.seconds, 60)
         h, m = divmod(m, 60)
