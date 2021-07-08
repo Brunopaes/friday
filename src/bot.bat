5c894ce0ad41c56fb732063d713f00203516d19c
bruno <brunopaes05@gmail.com>
2021-07-08 01:02:11 -0300

    Adding reddit blacklist

--- src/bot.bat
+++ src/bot.bat
@@ -2,8 +2,8 @@
 setlocal EnableExtensions
 
 :start_python_files
-E:
-cd E:\PythonProjects\Personal\friday\src
+D:
+cd D:\PythonProjects\Personal\friday\src
 
 python friday.py
 
@@ -12,6 +12,3 @@ call:infinite telegram_replier.py
 goto:check_python_files
 
 :infinite
-tasklist /FI "WINDOWTITLE eq %1 - %2" | findstr /c:PID > nul
-rem findstr /c:PID command added above to confirm that tasklist has found the process (errorlevel = 0). If not (errorlevel = 1).
-if %errorlevel% EQU 1 (start "%1" "%2")
