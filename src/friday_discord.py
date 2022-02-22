90d52918dcd80be5bfd5764c3c080cbfe235cae5
Brunopaes <brunopaes05@gmail.com>
2022-02-22 00:35:20 -0300

    polling

--- src/friday_discord.py
+++ src/friday_discord.py
@@ -40,6 +40,36 @@ async def hello_there(context):
 
 
 @friday.command(
+    name='poll',
+    description='General Kenobi',
+    pass_context=True
+)
+async def quick_poll(ctx, question, *options: str):
+    if len(options) <= 1:
+        await ctx.send('You need more than one option to make a poll!')
+        return
+    if len(options) > 10:
+        await ctx.send('You cannot make a poll for more than 10 things!')
+        return
+
+    if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
+        reactions = ['✅', '❌']
+    else:
+        reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
+
+    description = []
+    for x, option in enumerate(options):
+        description += '\n {} {}'.format(reactions[x], option)
+
+    embed = discord.Embed(title=question, description=''.join(description))
+    react_message = await ctx.send(embed=embed)
+
+    embed.set_footer(text='Poll ID: {}'.format(react_message.id))
+    await react_message.add_reaction("✅")
+    await react_message.add_reaction("❌")
+
+
+@friday.command(
     name='darth plagueis',
     aliases=(
             'darth', 'Darth', 'DARTH',
